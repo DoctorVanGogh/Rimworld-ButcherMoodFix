9ca12f85af2f6f2749993796a38e420257318f8c
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2017-07-07 08:55:43 +0200

    Slightly improved prisoner feed fix functionality

--- Source/RimworldButcherMoodFix/Patches/FoodUtility_BestFoodSourceOnMap_AnonStorey259.cs
+++ Source/RimworldButcherMoodFix/Patches/FoodUtility_BestFoodSourceOnMap_AnonStorey259.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Emit;
 using Harmony;
 using RimWorld;
+using Verse;
 
 namespace DoctorVanGogh.RimworldFixes.Patches {
 
     class FoodUtility_BestFoodSourceOnMap_AnonStorey259 {
 
-        public static bool IsBetterThanDesperate(IngestibleProperties p) {
-            return p?.preferability > FoodPreferability.DesperateOnly;
+        public static bool IsIngestibleBetterThanDesperate(Thing t) {
+            return (t is Building_NutrientPasteDispenser
+                           ? ThingDefOf.MealNutrientPaste
+                           : t.def).ingestible?.preferability > FoodPreferability.DesperateOnly;
         }
 
         /// <remarks>
@@ -25,35 +29,49 @@ namespace DoctorVanGogh.RimworldFixes.Patches {
              *      
              *  check with 
              *  
-             *      t.def.ingestible?.preferability > FoodPreferability.DesperateOnly
+             *      (t is Building_NutrientPasteDispenser ? ThingDefOf.MealNutrientPaste.ingestible : t.def.ingestible)?.preferability > FoodPreferability.DesperateOnly
              */
 
-            // extra NULL check introduces more bytes, and extra Labels - just substitute with method call and pad no-op
+             // extra checks introduce more bytes, and extra Labels - just substitute with method call and pad no-op
 
             /* IL Changes:
-                    [#I=Instruciton Index, A=Action, B=Byte size change, C=IL position/Label, Op=Opcode, Arg=Argument]
+                    [#I=Instruction Index, A=Action, B=Byte size change, C=IL position/Label, Op=Opcode, Arg=Argument]
               
 			        #I	A	B	C			Op			Arg 
 									        ...
-			        13			IL_0025:	ldarg.1
-			        14			IL_0026:	ldfld 		class Verse.ThingDef Verse.Thing::def
-			        15			IL_002b:	ldfld 		class RimWorld.IngestibleProperties Verse.ThingDef::ingestible
-			        16	<<<	-5	IL_0030:	ldfld 		valuetype RimWorld.FoodPreferability RimWorld.IngestibleProperties::preferability
-				        >>>	+5				call 		bool DoctorVanGogh.RimworldButcherMoodFix.Patches.FoodUtility_BestFoodSourceOnMap_AnonStorey259::IsBetterThanDesperate(class ['Assembly-CSharp']RimWorld.IngestibleProperties)
-			        17	<<<	-1	IL_0035: 	ldc.i4.2
-				        >>>	+1				nop
-			        18	<<<	-5	IL_0036: 	ble 		IL_0046
-				        >>>	+5				brfalse		IL_0046
-			        19						...
+					13			IL_0025:	ldarg.1
+					14	<<<	-5	IL_0026:	ldfld		class Verse.ThingDef Verse.Thing::def
+						>>>	+5				call		bool DoctorVanGogh.RimworldButcherMoodFix.Patches.FoodUtility_BestFoodSourceOnMap_AnonStorey259::IsIngestibleBetterThanDesperate(class ['Assembly-CSharp']Verse.Thing)
+					15	<<<	-5	IL_002b:	ldfld		class RimWorld.IngestibleProperties Verse.ThingDef::ingestible
+						>>>	+1				nop					
+						>>>	+1				nop					
+						>>>	+1				nop					
+						>>>	+1				nop					
+						>>>	+1				nop					
+					16	<<<	-5	IL_0030:	ldfld		valuetype RimWorld.FoodPreferability RimWorld.IngestibleProperties::preferability
+						>>>	+1				nop					
+						>>>	+1				nop					
+						>>>	+1				nop					
+						>>>	+1				nop					
+						>>>	+1				nop					
+					17	<<<	-1	IL_0035:	ldc.i4.2
+						>>>	+1				nop
+					18	<<<	-5	IL_0036:	ble			IL_0046
+						>>>	+5				brfalse		IL_0046
+					19						...
             */
 
             List<CodeInstruction> instructions = new List<CodeInstruction>(instr);
 
-            instructions[16] = new CodeInstruction(OpCodes.Call, typeof(FoodUtility_BestFoodSourceOnMap_AnonStorey259).GetMethod(nameof(IsBetterThanDesperate)));
-            instructions[17] = new CodeInstruction(OpCodes.Nop);
             var ci = instructions[18];
             instructions[18] = new CodeInstruction(OpCodes.Brfalse, ci.operand);        // 'old' operand contains label to use
-
+            instructions[17] = new CodeInstruction(OpCodes.Nop);
+            instructions.RemoveAt(16);
+            instructions.InsertRange(16, Enumerable.Repeat(new CodeInstruction(OpCodes.Nop), 5));
+            instructions.RemoveAt(15);
+            instructions.InsertRange(15, Enumerable.Repeat(new CodeInstruction(OpCodes.Nop), 5));
+            instructions[14] = new CodeInstruction(OpCodes.Call, typeof(FoodUtility_BestFoodSourceOnMap_AnonStorey259).GetMethod(nameof(IsIngestibleBetterThanDesperate)));
+ 
             return instructions;
         }
     }
